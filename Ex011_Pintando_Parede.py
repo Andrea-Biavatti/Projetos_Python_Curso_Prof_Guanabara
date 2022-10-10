7929b04aa50a947f800d80712d46d9745589d5c4
ANDRÉA BIAVATTI <72770754+Andrea-Biavatti@users.noreply.github.com>
2022-10-10 14:58:54 -0300

    Delete Ex011_Pintando_Parede.py

--- Ex011_Pintando_Parede.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que leia a largura e a altura de uma parede em metros, 
-# calcule a sua área e a quantidade de tinta necessária para pintá-la, 
-# sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
-
-lg = float(input('Digite a largura da parede: '))
-alt = float(input('Digite a altura da parede: '))
-a = lg * alt
-t = a / 2
-print('A área a ser pintada é de {:.1f} m². Você precirá de {:.2f} litros de tinta.'.format(a, t))
