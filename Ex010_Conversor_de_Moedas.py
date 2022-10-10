1187d21670ceebcea0af966fa8093f20ecc09a1d
ANDRÉA BIAVATTI <72770754+Andrea-Biavatti@users.noreply.github.com>
2022-10-10 14:58:47 -0300

    Delete Ex010_Conversor_de_Moedas.py

--- Ex010_Conversor_de_Moedas.py
+++
@@ -1,5 +0,0 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
-
-d = float(input('Quantos reais você possui? R$ '))
-dol = d / 3.27
-print('Vocé poderá comprar US$ {:.2f} .'.format(dol))
