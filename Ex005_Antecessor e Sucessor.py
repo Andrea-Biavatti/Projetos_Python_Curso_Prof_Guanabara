7fa6c018166a161a14b960fd4a03e66d476811f4
ANDRÉA BIAVATTI <72770754+Andrea-Biavatti@users.noreply.github.com>
2022-10-10 14:56:45 -0300

    Delete Ex005_Antecessor e Sucessor.py

--- Ex005_Antecessor e Sucessor.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
-
-n = int(input('Digite um número: '))
-a = n - 1
-s = n + 1
-print('O número digitado foi {}. O número antecessor é {} e o sucessor é {}'.format(n, a, s))
-
-n = int(input('Digite um número: '))
-print('O número digitado foi {}. O número antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
