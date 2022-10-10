11fae795bfcf57807917955933f893fa2e21bb7c
ANDRÉA BIAVATTI <72770754+Andrea-Biavatti@users.noreply.github.com>
2022-10-10 14:59:13 -0300

    Delete Ex014_Conversor_de_Temperaturas.py

--- Ex014_Conversor_de_Temperaturas.py
+++
@@ -1,5 +0,0 @@
-# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
-
-temp = float(input('Informe a temperatura em C: '))
-f = ((9 * temp) / 5) + 32
-print('A temperatua de {} C corresponde a {} F!'.format(temp, f))
